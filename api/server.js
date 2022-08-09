86ae257bbc1d35437de46f04140c9c5f543a592a
Matt Decker <matt@Matts-MBP.lan>
2022-08-09 11:04:33 -0400

    9 pass

--- api/server.js
+++ api/server.js
@@ -3,12 +3,30 @@ const express = require('express')
 const User = require('./users/model')
 const server = express()
 
-// server.use(express.json())
+server.use(express.json())
 
 server.get('/api/users', (req, res) => {
     User.find()
         .then(users => {
             res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+            })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            }
+            res.json(user)
         })
         .catch(err => {
             res.status(500).json({
@@ -18,17 +36,23 @@ server.get('/api/users', (req, res) => {
         })
 })
 
-server.get('/api/users/:id', (req, res) => {
-    User.findById(req.params.id)
-        .then(user => {
-            res.json(user)
+server.post('/api/users', (req, res) => {
+    if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
         })
-        .catch(err => {
-            res.status(500).json({
-                message: 'error getting users',
-                err: err.message,
+    } else {
+        User.insert(req.body)
+            .then(result => {
+                res.status(201).json(result)
             })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'error creating user',
+                    err: err.message,
+                })
         })
+    }
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
